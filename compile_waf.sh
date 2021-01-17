7f52f3880d095ae26321cff6c18f28d0ff674496
gaurav-dixitv <gaurav.dixitv@gmail.com>
2021-01-17 15:02:13 -0800

    Added test sferes experiment.

--- compile_waf.sh
+++ compile_waf.sh
@@ -2,10 +2,14 @@
 
 mkdir install
 
+# experiments
+cd libs/sferes2/exp
+ln -s ../../../experiments/sferes
+cd ../../../
 
+# modules
 cd  libs/sferes2/modules
 ln -s ../../cvt_map_elites
-
 cd ../../../
 
 echo "cvt_map_elites" >> libs/sferes2/modules.conf
@@ -18,3 +22,9 @@ echo "Install directory: ${INSTALL}"
 cd libs/sferes2
 ./waf configure --cpp14=yes
 ./waf -v
+
+
+# compile experiments
+# 1. sferes test
+./waf configure --cpp14=yes --exp sferes
+./waf --exp sferes
